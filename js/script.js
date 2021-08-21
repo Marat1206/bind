bd0a9a477b649db77d01c97ea838a1ca8b9699bf
Marat1206 <marikzhekov@gmail.com>
2021-08-21 20:01:29 +0300

    apdate

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
 'use script'
 
 let obj = {
-    name: 'marat'
+    name: 'marat',
+    weight: '75',
 }
 
-function someFunc(lastName, age){
-    console.log((`Имя: ${this.name}, Фамилия: ${lastName}, Возвраст: ${age}`));
+function info(phone, lastName, age){
+    console.log(`Имя: ${this.name}, Фамилия: ${lastName}, Телефон: ${phone}, Вес: ${this.weight}, Возвраст: ${age}, `);
 }
 
-function bind(func, context, ...rest){
-    return function (){
-        const ID = Date.now().toString();
 
-        context[ID] = func;
+let bind = function(func, context) {
+    let bindArgs = [].slice.call(arguments,2 );
+    return function() {
+        let fnArgs = [].slice.call(arguments);
+        return func.apply(context, bindArgs.concat(fnArgs));
+    };
+};
 
-        const result = context[ID](...rest);
+let result = bind(info, obj, '1234', 'zhekov', '25')();
 
-    }
-}
 
-bind(someFunc, obj, 'Zhekov', '25')();
+
+
+
+
